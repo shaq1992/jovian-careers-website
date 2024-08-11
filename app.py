fd44db93f503dc09d2902088e98a11234e43d028
Shadman Qaisar <106549404+shaq1992@users.noreply.github.com>
2024-08-11 09:15:52 +0000

    Added job items

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+}, {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "New Delhi, India",
+    "salary": "Rs. 15,00,000"
+}, {
+    "id": 3,
+    "title": "Backend Engineer",
+    "location": "San Francisco, USA"
+}, {
+    "id": 4,
+    "title": "Prompt Engineer",
+    "location": "Remote (US)",
+    "salary": "$ 500,000"
+}]
+
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
